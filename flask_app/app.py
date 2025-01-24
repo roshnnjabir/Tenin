16a04168b77794d9f45e1d59f4b378c221a4a703
Roshnn <roshanjabir7@gmail.com>
2025-01-24 14:59:07 +0530

    Shop, Contact Us Pages Added, Functionallity To Be Done...

--- flask_app/app.py
+++ flask_app/app.py
@@ -10,5 +10,15 @@ def home():
 def shop():
     return render_template('shop.html')
 
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ flask_app/auth.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+from flask_login import login_manager
+from models import User
+from extensions import db
+
